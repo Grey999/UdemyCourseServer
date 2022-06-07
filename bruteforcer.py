eea503ca78a911d16eb151411492d20b88e1c4cd
Coline Berrue <coline.berrue@epita.fr>
2022-06-07 10:13:17 +0200

    coding the different options of the bruteforcer

--- bruteforcer.py
+++ bruteforcer.py
@@ -11,13 +11,39 @@ class Bruteforcer:
         print("*  Bruteforcer by Thanatos   *")
         print("##############################")
 
-    def start(self, argv):
-        print("start")
 
+    def launcher_thread(self, passwords, threads, user, url):
+        global list
+        list = []
+
+
+    def start(self, argv):
+        self.print_banner()
+        try:
+            opts, args = getopt.getopt(argv, "u:w:f:t")
+        except getopt.GetoptError:
+            print("Error on arguments")
+            sys.exit()
+        for opt,arg in opts:
+            if opt == "-u":
+                user = arg
+            elif opt == "-w":
+                url = arg
+            elif opt == "-f":
+                passlist = arg
+            elif opt == "-t":
+                threads = arg
+        try:
+            f = open(passlist, "r")
+            passwords = f.readlines()
+        except:
+            print("[!!] Can't open the file !!")
+            sys.exit()
+        self.launcher_thread(passwords,threads, user, url)
 
     def main(self):
         if __name__ == "__main__":
             try:
                 self.start(sys.argv)
             except KeyboardInterrupt:
-                print("Interrupted")
+                print("[!!]Interrupted")
